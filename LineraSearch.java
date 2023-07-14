ceb34a328fa5ce87c2fa92c97328d5842905cf3c
Madanlatha <madanganesan08@gmail.com>
2023-07-14 06:03:21 +0530

    linearSearch

---
+++ LineraSearch.java
@@ -0,0 +1,36 @@
+package search;
+
+import java.util.Scanner;
+
+public class LineraSearch {
+	
+		public static int LinearSearch(int arr[],int k) {
+			int size=arr.length;
+			int count=0;
+			for(int i=0;i<size;i++) {
+				if(arr[i]==k) {
+					count++;
+				}
+				else {
+					count=0;
+				}
+			}
+			return count;
+			
+		}
+		public static void main(String[] args) {
+			Scanner sc = new Scanner(System.in);
+			int n=sc.nextInt();
+			int arr[] =new int[n];
+			for(int i=0;i<n;i++) {
+				arr[i]=sc.nextInt();
+			}
+			int k=sc.nextInt();
+			int value=LinearSearch(arr,k);
+			System.out.println(value);
+
+		}
+
+	}
+
+
